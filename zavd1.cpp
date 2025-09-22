66f04da8c294d068acf82e1a58528071d28f94b9
kramaranna <kramaranna123@gmail.com>
2025-09-22 13:46:21 +0300

    Завдання 1

---
+++ zavd1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    long int i=10, s=0;
+        while (i<=100)
+        {
+            i=i+1;
+          s=s+i*i;
+        }
+    cout<<"\nСума квадратів від 10 до 100: = "<<s;
+    return 0;
+}

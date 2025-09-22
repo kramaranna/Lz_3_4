ea3c5f037933632058f8ac5626fa9eaa5c62ebd0
kramaranna <kramaranna123@gmail.com>
2025-09-22 15:17:16 +0300

    Завдання 2

---
+++ zavd2 (1).cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    double i=1, a=1, b=6, k, x, y, h;
+    cout<<"\nВведіть k: ";
+    cin>>k;
+    h=(b-a)/k;
+        for (i=a; i<=b; i=i+1)
+        {
+          x=a+i*h;
+          y=cos(x*x+4*log(x));
+          cout<<"\n Координати "<<i<<": "<<"("<<x<<";"<<y<<")";
+        }
+    return 0;
+}
